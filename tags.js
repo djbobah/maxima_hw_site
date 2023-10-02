0df4f24457313a3ba71b79af37f22d41359954a1
Vladimir <djbobah@mail.ru>
2023-10-02 11:24:29 +0300

    проставил теги и ссылки, добавил смену курсора при наведении на кнопку

---
+++ tags.js
@@ -0,0 +1,19 @@
+// header x1       1  +
+// footer x1       1  +
+// nav x3          3  +
+// section x6      6  +
+//article x10     10
+//div x5          5
+// h1 x1           1  +
+// h2 x5           5  +
+// h3 x10          10
+// p x14           13
+// ul x1           1  +
+// li x8           8  +
+// a x15           15
+// img x10
+// button x8       7
+// span x11        11
+
+// mark x8         8  +
+// iframe x1       1  +
